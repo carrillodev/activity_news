dfcfc06517a4b741de239bbdccb540ecb192480a
Felipe Carrillo <felipe.carrillo1@hotmail.com>
2021-10-12 12:48:47 -0500

    Add bean storage service to configuration file.

--- src/main/java/com/example/news/configuration/NewConfig.java
+++ src/main/java/com/example/news/configuration/NewConfig.java
@@ -4,6 +4,8 @@ import com.example.news.domain.ports.api.NewServicePort;
 import com.example.news.domain.ports.spi.NewPersistencePort;
 import com.example.news.domain.service.NewServiceImpl;
 import com.example.news.infraestructure.adapters.NewJpaAdapter;
+import com.example.news.storage.StorageService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -17,5 +19,13 @@ public class NewConfig {
     @Bean
     public NewServicePort newService() {
         return new NewServiceImpl(newPersistence());
+    }
+
+    @Bean
+    CommandLineRunner init(StorageService storageService) {
+        return (args) -> {
+            storageService.deleteAll();
+            storageService.init();
+        };
     }
 }
