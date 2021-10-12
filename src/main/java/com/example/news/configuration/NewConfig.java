897c43b4284a4bb26c49c51f21b5592ce8893970
Felipe Carrillo <felipe.carrillo1@hotmail.com>
2021-10-12 16:24:25 -0500

    Add storage service bean and enable configuration properties.

--- src/main/java/com/example/news/configuration/NewConfig.java
+++ src/main/java/com/example/news/configuration/NewConfig.java
@@ -4,12 +4,15 @@ import com.example.news.domain.ports.api.NewServicePort;
 import com.example.news.domain.ports.spi.NewPersistencePort;
 import com.example.news.domain.service.NewServiceImpl;
 import com.example.news.infraestructure.adapters.NewJpaAdapter;
+import com.example.news.storage.StorageProperties;
 import com.example.news.storage.StorageService;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@EnableConfigurationProperties(StorageProperties.class)
 public class NewConfig {
     @Bean
     public NewPersistencePort newPersistence() {
